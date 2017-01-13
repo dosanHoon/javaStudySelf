e1943d0d72f05f4ffdd470a431e656c09ceab852
shhan <shhan@maktb.com>
2017-01-13 12:59:43 +0900

    connection pool
    <Resource name="jdbc/mytest"
    	auth="Container"
    	type="javax.sql.DataSource"
           driverClassName="com.mysql.jdbc.Driver"
           url="jdbc:mysql://192.168.0.201/test"
           username="username"
           password="password"
           maxActive="5"
           maxIdle="3"
           maxWait="1000"
    />
    
    servers server.xml 수정

--- PJ02/src/com/funple/ch5/ViewServlet.java
+++ PJ02/src/com/funple/ch5/ViewServlet.java
@@ -1,6 +1,7 @@
 package com.funple.ch5;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -8,6 +9,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.funple.ch4.JdbcTest;
+import com.funple.ch6.ConnectionPool;
 
 public class ViewServlet extends HttpServlet{
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -18,6 +22,19 @@ public class ViewServlet extends HttpServlet{
 		
 		request.setAttribute("msg", msg);
 		request.setAttribute("tlist", testlist);
+		
+		ConnectionPool pool = new ConnectionPool();
+		
+		try {
+			Connection poolConn =  pool.getDBCPConnection();
+			System.out.println("poolConn : " + poolConn.getCatalog());
+			JdbcTest jt = new JdbcTest();
+			jt.viewTable(poolConn);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/view.jsp");
 		rd.forward(request, response);
 	}
